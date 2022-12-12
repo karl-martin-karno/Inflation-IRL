5ab934c2949c14287bf3bbff1f0154eaefdf9562
Karl Erik Karindi <karl.karindi@gmail.com>
2022-12-12 22:53:13 +0200

    Format MainActivity

--- app/src/main/java/com/example/inflation_irl/MainActivity.kt
+++ app/src/main/java/com/example/inflation_irl/MainActivity.kt
@@ -2,15 +2,12 @@ package com.example.inflation_irl
 
 import android.Manifest
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.inflation_irl.databinding.ActivityMainBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mapLocation: MapLocation;
@@ -30,20 +27,31 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun checkPermission(permission: String, requestCode: Int) {
-        if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_DENIED) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                permission
+            ) == PackageManager.PERMISSION_DENIED
+        ) {
             ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
         } else {
-            Toast.makeText(applicationContext, "Permission already granted", Toast.LENGTH_SHORT).show()
+            Toast.makeText(applicationContext, "Permission already granted", Toast.LENGTH_SHORT)
+                .show()
         }
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<String>,
-                                            grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == MY_PERMISSIONS_REQUEST_LOCATION) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(applicationContext, "Location Permission Granted", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    applicationContext,
+                    "Location Permission Granted",
+                    Toast.LENGTH_SHORT
+                ).show()
             } else {
                 Toast.makeText(this, "Location Permission Denied", Toast.LENGTH_SHORT).show()
             }
