11e303fbbe4f5f8ef1e5cab58fd35cc9f3957fc0
Kaspar Kadalipp <kaspar.kadalipp@ut.ee>
2022-12-18 20:14:25 +0200

    Disable canceling the prompt

--- app/src/main/java/com/example/inflation_irl/views/GridLayoutDialogFragment.kt
+++ app/src/main/java/com/example/inflation_irl/views/GridLayoutDialogFragment.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.example.inflation_irl.R
+import com.example.inflation_irl.Store
 
-class GridLayoutDialogFragment : DialogFragment() {
-
+class GridLayoutDialogFragment(val selectStore: (store:Store) -> Unit) : DialogFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // inflate the grid layout view using the .xml file
-        return inflater.inflate(R.layout.select_store_prompt, container, false)
+        isCancelable = false
+
+        val view = inflater.inflate(R.layout.select_store_prompt, container, false)
+        view.findViewById<ImageView>(R.id.select_store_prisma).setOnClickListener{
+            Toast.makeText(requireContext(),"Toggle prompt:", Toast.LENGTH_SHORT).show()
+            selectStore(Store.PRISMA)
+            dismiss()
+        }
+//        view.findViewById<ImageView>(R.id.select_store_rimi).setOnClickListener{}
+//        view.findViewById<ImageView>(R.id.select_store_maxima).setOnClickListener{}
+//        view.findViewById<ImageView>(R.id.select_store_selver).setOnClickListener{}
+        return view
     }
 }
