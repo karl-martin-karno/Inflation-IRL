83d211a395a7c221c256827be0cf406cc8656d88
Kaspar Kadalipp <kaspar.kadalipp@ut.ee>
2022-12-18 10:58:24 +0200

    Add view binding

--- app/src/main/java/com/example/inflation_irl/views/ProductInfoFragment.kt
+++ app/src/main/java/com/example/inflation_irl/views/ProductInfoFragment.kt
@@ -1,13 +1,19 @@
 package com.example.inflation_irl.views
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import com.example.inflation_irl.Product
 import com.example.inflation_irl.R
+import com.example.inflation_irl.Store
+import com.example.inflation_irl.databinding.FragmentProductInfoBinding
+import com.example.inflation_irl.databinding.FragmentScanBarcodeBinding
 
 
 /**
@@ -17,18 +23,24 @@ import com.example.inflation_irl.R
  */
 class ProductInfoFragment : Fragment() {
 
+    private var _binding: FragmentProductInfoBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_product_info, container, false)
+
+        _binding = FragmentProductInfoBinding.inflate(inflater, container, false)
+        val view = binding.root
+
         val icon = arguments?.getInt("icon") ?: R.drawable.red_bull
         val store = arguments?.getInt("store") ?: R.drawable.rimi
         val title = arguments?.getString("title") ?: ""
 
-        view.findViewById<ImageView>(R.id.product_info_item_icon).setImageResource(icon)
-        view.findViewById<ImageView>(R.id.product_info_store_icon).setImageResource(store)
-        view.findViewById<TextView>(R.id.product_info_title).text = title
+        binding.productInfoItemIcon.setImageResource(icon)
+        binding.productInfoStoreIcon.setImageResource(store)
+        binding.productInfoTitle.text = title
         return view
     }
 
