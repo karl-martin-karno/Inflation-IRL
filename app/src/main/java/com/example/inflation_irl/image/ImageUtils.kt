9ec63fbcec9d2cea8c28e01f942b8d34e7d30072
Kaspar Kadalipp <kaspar.kadalipp@ut.ee>
2022-12-18 10:13:37 +0200

    Fix image orientation

--- app/src/main/java/com/example/inflation_irl/image/ImageUtils.kt
+++ app/src/main/java/com/example/inflation_irl/image/ImageUtils.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Bitmap.createScaledBitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.os.Environment
 import android.util.Log
+import androidx.exifinterface.media.ExifInterface
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.withContext
 import java.io.File
@@ -13,12 +15,32 @@ import java.io.File
 class ImageUtils {
     suspend fun createScaledImage(imagePath: String): Bitmap {
         return withContext(IO) {
-            val bmp = BitmapFactory.decodeFile(imagePath)
+            var bmp = BitmapFactory.decodeFile(imagePath)
             val ratio = bmp.width.toDouble() / bmp.height
+            bmp = fixOrientation(bmp, imagePath)
             createScaledBitmap(bmp, (800 * ratio).toInt(), 800, false)
         }
     }
 
+    private fun fixOrientation(bitmap: Bitmap, url: String): Bitmap {
+        val exif = ExifInterface(url)
+
+        return when (exif.getAttributeInt(
+            ExifInterface.TAG_ORIENTATION,
+            ExifInterface.ORIENTATION_NORMAL
+        )) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> rotateBitmap(bitmap, 90)
+            ExifInterface.ORIENTATION_ROTATE_180 -> rotateBitmap(bitmap, 180)
+            ExifInterface.ORIENTATION_ROTATE_270 -> rotateBitmap(bitmap, 270)
+            else -> bitmap
+        }
+    }
+
+    private fun rotateBitmap(bitmap: Bitmap, degrees: Int): Bitmap {
+        val matrix = Matrix()
+        matrix.postRotate(degrees.toFloat())
+        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, false)
+    }
     /**
      * Returns the File for a photo stored on disk given the fileName.
      * Creating the storage directory if it does not exist
