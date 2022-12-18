fe9c7c0f87dc6f9f413168839677282548255cdb
Kaspar Kadalipp <kaspar.kadalipp@ut.ee>
2022-12-18 02:18:59 +0200

    Add onClickListener to button

--- app/src/main/java/com/example/inflation_irl/views/ScanBarcodeFragment.kt
+++ app/src/main/java/com/example/inflation_irl/views/ScanBarcodeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import com.example.inflation_irl.R
 
 
@@ -22,7 +24,12 @@ class ScanBarcodeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_scan_barcode, container, false)
+        val view = inflater.inflate(R.layout.fragment_scan_barcode, container, false)
+
+        view.findViewById<Button>(R.id.scan_barcode_button).setOnClickListener{
+            Toast.makeText(requireContext(),"Scan Barcode clicked",Toast.LENGTH_SHORT).show();
+        }
+        return view
     }
 
 }
