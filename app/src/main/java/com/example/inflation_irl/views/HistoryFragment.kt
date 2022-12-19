dd08f60421f108960af1a6ba73ffee22b83a1a09
Karl Erik Karindi <karl.karindi@gmail.com>
2022-12-19 07:07:29 +0200

    Use db data instead of dummy data in history view

--- app/src/main/java/com/example/inflation_irl/views/HistoryFragment.kt
+++ app/src/main/java/com/example/inflation_irl/views/HistoryFragment.kt
@@ -1,16 +1,20 @@
 package com.example.inflation_irl.views
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.example.inflation_irl.Product
 import com.example.inflation_irl.R
 import com.example.inflation_irl.Store
 import com.example.inflation_irl.adapter.HistoryListAdapter
 import com.example.inflation_irl.adapter.HistoryListItem
-
+import com.example.inflation_irl.dao.FireStoreDao
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
 
 /**
  * A simple [Fragment] subclass.
@@ -18,52 +22,41 @@ import com.example.inflation_irl.adapter.HistoryListItem
  * create an instance of this fragment.
  */
 class HistoryFragment : Fragment() {
+
+    private val fireStoreDao = FireStoreDao()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_history, container, false)
 
-        val dataset = getDummyData()
-        val recyclerView = view.findViewById<RecyclerView>(R.id.history_recyclerview)
-        recyclerView.adapter = HistoryListAdapter(dataset)
+        CoroutineScope(IO).launch {
+            fireStoreDao.getProducts { products ->
+                val dataset = mapDataSetToHistoryList(products)
+                val recyclerView = view.findViewById<RecyclerView>(R.id.history_recyclerview)
+                recyclerView.adapter = HistoryListAdapter(dataset)
+                recyclerView.setHasFixedSize(true)
+            }
+        }
         return view
     }
 
-    // TODO replace with query
-    private fun getDummyData(): MutableList<HistoryListItem> {
+    private fun mapDataSetToHistoryList(products: List<Product>): MutableList<HistoryListItem> {
         val dataset = mutableListOf<HistoryListItem>()
-        dataset.add(
-            HistoryListItem(
-                barcode = "8710398523983",
-                title = "Doritos Nacho Cheese Flavored Tortilla Chips",
-                iconUrl = "https://s3-eu-west-1.amazonaws.com/balticsimages/images/180x220/3f4dac76479e9d075db6b662f46c251b.png",
-                store = Store.PRISMA.name
-            )
-        )
-        dataset.add(
-            HistoryListItem(
-                barcode = "5060335636225",
-                title = "Monster Energy Zero Sugar Energy Drink",
-                iconUrl = "https://s3-eu-west-1.amazonaws.com/balticsimages/images/180x220/89e4630f3cf26fb4b0c620e27eb01134.png",
-                store = Store.PRISMA.name
-            )
-        )
-        dataset.add(
-            HistoryListItem(
-                barcode = "6414893149526",
-                title = "Pitsa Hawai Dolce singi-ananassi-sinihalitusjuustuga, 340 g",
-                iconUrl = "https://s3-eu-west-1.amazonaws.com/balticsimages/images/180x220/fae82dcf30a89e555efb1dfa65d1eb83.png",
-                store = Store.KAUBAMAJA.name
-            )
-        )
-        dataset.add(
-            HistoryListItem(
-                barcode = "9002490100490",
-                title = "Red Bull Energy Drink 4x 0.25L",
-                iconUrl = "https://s3-eu-west-1.amazonaws.com/balticsimages/images/180x220/77d9d03eec79bfb50882db7cff50f090.png",
-                store = Store.KAUBAMAJA.name
-            )
-        )
+        val usedBarCodes = mutableListOf<String>()
+        products.forEach { product ->
+            if (product.barCode !in dataset.map { it.barcode }) {
+                dataset.add(
+                    HistoryListItem(
+                        product.barCode ?: "",
+                        product.name.toString(),
+                        product.imageFilePath,
+                        product.store.toString()
+                    )
+                )
+            }
+            usedBarCodes.add(product.barCode ?: "")
+        }
         return dataset
     }
 }
