e27a1df24b2f76787bb25d25d24e1eed65869e28
Karl Erik Karindi <karl.karindi@gmail.com>
2022-12-19 03:06:00 +0200

    Improve store detection

--- app/src/main/java/com/example/inflation_irl/location/LocationUtils.kt
+++ app/src/main/java/com/example/inflation_irl/location/LocationUtils.kt
@@ -5,45 +5,49 @@ import android.content.Context
 import android.util.Log
 import com.example.inflation_irl.Store
 import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Tasks
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.launch
 
 class LocationUtils {
-    val TAG = "MyMapsActivity"
+    val TAG = "LocationUtils"
     val acceptedPlaces: Array<String> = arrayOf("selver", "prisma")
 
+    fun interface LocationHandler {
+        fun onStoreFound(store: Store)
+    }
+
     @SuppressLint("MissingPermission")
-    suspend fun findNearestStore(applicationContext: Context): Store? {
-        val match = withContext(IO) {
-            Places.initialize(applicationContext, "AIzaSyAViygAFms699z-JFoWsDB9MzVTegVHfQ4")
-            val placesClient = Places.createClient(applicationContext)
+    fun findNearestStore(context: Context, locationHandler: LocationHandler) {
+        CoroutineScope(IO).launch {
+            Log.d(TAG, "Searching for stores: ")
+            Places.initialize(context, "AIzaSyAViygAFms699z-JFoWsDB9MzVTegVHfQ4")
+            val placesClient = Places.createClient(context)
             val placeFields: List<Place.Field> = listOf(Place.Field.NAME)
             val request: FindCurrentPlaceRequest = FindCurrentPlaceRequest.newInstance(placeFields)
             val placeResponse = placesClient.findCurrentPlace(request)
-            var finalMatch: Store? = null
             placeResponse.addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     val response = task.result
                     val placesFound = response.placeLikelihoods.map { it.place.name }
-                    run lit@{
-                        placesFound.forEach { place ->
-                            val match =
-                                acceptedPlaces.filter { place.contains(it, ignoreCase = true) }
-                            if (match.size > 0) {
-                                Log.i(TAG, place)
-                                when (match.get(0)) {
-                                    "selver" -> finalMatch = Store.SELVER
-                                    "prisma" -> finalMatch = Store.PRISMA
-                                }
-                                return@lit;
+                    Log.d(TAG, "Places found: $placesFound")
+                    placesFound.forEach { place ->
+                        val match =
+                            acceptedPlaces.filter {
+                                place.lowercase().contains(it, ignoreCase = true)
+                            }
+                        if (match.isNotEmpty()) {
+                            Log.i(TAG, place)
+                            when (match.get(0)) {
+                                "selver" -> locationHandler.onStoreFound(Store.SELVER)
+                                "prisma" -> locationHandler.onStoreFound(Store.PRISMA)
                             }
                         }
                     }
+
                 } else {
                     val exception = task.exception
                     if (exception is ApiException) {
@@ -51,11 +55,6 @@ class LocationUtils {
                     }
                 }
             }
-            Tasks.await(placeResponse);
-            delay(1000) // Idiootsus kuna ma ei tea kuidas siin funktsioonis mingi Promise moodi asja tekitada :)
-            Log.i(TAG, finalMatch.toString())
-            finalMatch
         }
-        return match;
     }
 }
