2e69d84de554a3c0601d7bc917876aeaf066ba15
Karl Erik Karindi <karl.karindi@gmail.com>
2022-12-19 06:43:01 +0200

    Pass actual barcode instead of dummy data

--- app/src/main/java/com/example/inflation_irl/views/ScanBarcodeFragment.kt
+++ app/src/main/java/com/example/inflation_irl/views/ScanBarcodeFragment.kt
@@ -140,7 +140,7 @@ class ScanBarcodeFragment : Fragment() {
         if (viewModel.selectedStore == Store.PRISMA) {
             val bundle = bundleOf(
                 "store" to Store.PRISMA.name,
-                "barcode" to "4743050000045",
+                "barcode" to binding.productBarCodeEditText.text.toString(),
                 "navigation" to "barcodeView"
             )
             findNavController(view).navigate(R.id.productInfoFragment2, bundle)
